c6c59ebdefb3d95f15756619f4011069463fc0b2
Piotr Jander <pjander2@gmail.com>
2021-05-04 13:48:35 +0200

    Updates setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def main():
         author='neptune.ai',
         support='contact@neptune.ai',
         author_email='contact@neptune.ai',
-        url="https://github.com/neptune-ai/neptune-client",
+        url="https://github.com/neptune-ai/neptune-tensorflow-keras",
         long_description=readme,
         long_description_content_type="text/markdown",
         license='MIT License',
