ed999acb62cd6af7ba7bd38931f99838531f789a
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-11-22 14:56:22 +0100

    use subset of mnist to save RAM

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,13 @@ def dataset():
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    x_train = x_train[:3000, :, :]
+    y_train = y_train[:3000]
+
+    x_test = x_test[:500, :, :]
+    y_test = y_test[:500]
+
     return (x_train, y_train), (x_test, y_test)
 
 
